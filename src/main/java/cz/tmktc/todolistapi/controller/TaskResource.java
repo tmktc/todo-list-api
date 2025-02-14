a26455df02aea1f6859f7847f6bd84a9ece900ae
tmktc <tmktc.git@gmail.com>
2025-02-14 11:47:05 +0100

    TaskService, TaskResource: add javadoc

--- src/main/java/cz/tmktc/todolistapi/controller/TaskResource.java
+++ src/main/java/cz/tmktc/todolistapi/controller/TaskResource.java
@@ -19,21 +19,45 @@ import java.util.List;
 public class TaskResource {
     private final TaskService taskService;
 
+    /**
+     * Returns all tasks.
+     *
+     * @return all tasks
+     */
     @GetMapping
     public ResponseEntity<List<Task>> getAllTasks() {
         return ResponseEntity.ok().body(taskService.getAllTasks());
     }
 
+    /**
+     * Returns a task.
+     *
+     * @param id id of a task
+     * @return task whose id matches the parameter
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable(value = "id") String id) {
         return ResponseEntity.ok().body(taskService.getTask(id));
     }
 
+    /**
+     * Creates a task.
+     * If ID is included in the request, it updates the given task instead.
+     *
+     * @param task to be created or updated
+     * @return created or updated task
+     */
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
         return ResponseEntity.created(URI.create("/tasks/userID")).body(taskService.createTask(task));
     }
 
+    /**
+     * Deletes a task.
+     *
+     * @param id id of the task that is to be deleted
+     * @return 204 noContent if the deletion is successful, 404 notFound if the deletion failed
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable(value = "id") String id) {
         boolean successfulDeletion = taskService.deleteTask(id);
--- src/main/java/cz/tmktc/todolistapi/service/TaskService.java
+++ src/main/java/cz/tmktc/todolistapi/service/TaskService.java
@@ -17,18 +17,41 @@ import java.util.List;
 public class TaskService {
     private final TaskRepository taskRepository;
 
+    /**
+     * Returns all tasks.
+     *
+     * @return all tasks
+     */
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
 
+    /**
+     * Returns a task.
+     *
+     * @param id id of a task
+     * @return task whose id matches the parameter
+     */
     public Task getTask(String id) {
         return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
     }
 
+    /**
+     * Creates or updates a task.
+     *
+     * @param task to be creates or updated
+     * @return created or updated task
+     */
     public Task createTask(Task task) {
         return taskRepository.save(task);
     }
 
+    /**
+     * Deletes a task.
+     *
+     * @param id id of the task
+     * @return true if the deletion was successful, false if not
+     */
     public boolean deleteTask(String id) {
         try {
             taskRepository.delete(getTask(id));
